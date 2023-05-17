605841c1a3e40e517312e503f497fbe6eefda71a
xErik <thecoffeejunkie@hotmail.de>
2023-05-17 15:14:02 +0200

    demo: added missing dependency

--- example/flutter_example/lib/main.dart
+++ example/flutter_example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             toolbarHeight: 0,
